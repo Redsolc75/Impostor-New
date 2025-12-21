52dec1535f563f78fb4057b7a39df936f53de5ab
Redsolc75 <redsolc@gmail.com>
2025-12-21 19:31:05 +0100

    Update base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,8 +1,8 @@
 import { createClient } from '@base44/sdk';
 // import { getAccessToken } from '@base44/sdk/utils/auth-utils';
 
-// Create a client with authentication required
+// Hem canviat 'requiresAuth' a false perquè no t'expulsi
 export const base44 = createClient({
-  appId: "6946651c132cf0403fd62248", 
-  requiresAuth: true // Ensure authentication is required for all operations
+  appId: "6946651c132cf0403fd62248",
+  requiresAuth: false 
 });
